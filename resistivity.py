5279dd18834962845c9799b4509fe7ad0ccaefec
Nico <nwillen@college.harvard.edu>
2026-02-23 11:42:16 -0500

    recent general updates

---
+++ resistivity.py
@@ -0,0 +1,20 @@
+# Graph resisitivity vs temperature for our sample
+
+# Import libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import .txt file
+
+df = pd.read_csv("resistivity/Sample_Trial_5.csv", sep=",")
+
+temp = df["Temp"]
+resist = df["Volts(uV)"]/54.8
+
+plt.figure()  
+plt.scatter(temp, resist)
+plt.xlabel("Temperature (K)")
+plt.ylabel("Resistivity (Ohm)")
+plt.title("Resistivity vs Temperature")
+
+plt.show()
